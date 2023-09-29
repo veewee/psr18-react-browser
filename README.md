c18a828a5a4591b74642cf77e5a58c25c71da8f8
Toon Verwerft <toonverwerft@gmail.com>
2023-09-29 13:47:51 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,2 @@
+# A PSR-18 Bridge for React HTTP browser
+
---
+++ src/Psr18ReactBrowserClient.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace Veewee\Psr18ReactBrowser;
+
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use React\Http\Browser;
+use function React\Async\await;
+
+final class Psr18ReactBrowserClient implements ClientInterface
+{
+    public function __construct(
+        private readonly Browser $browser
+    ){
+    }
+
+    public static function default(): self
+    {
+        return new self(new Browser());
+    }
+
+    public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        return await(
+            $this->browser->request(
+                $request->getMethod(),
+                (string) $request->getUri(),
+                $request->getHeaders(),
+                (string) $request->getBody()
+            )
+        );
+    }
+}

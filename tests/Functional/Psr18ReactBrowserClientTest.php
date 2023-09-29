c1122d91720214c05bc2f788bef4b45717d1b182
Toon Verwerft <toonverwerft@gmail.com>
2023-09-29 14:29:22 +0200

    Improve tests

--- tests/Functional/Psr18ReactBrowserClientTest.php
+++ tests/Functional/Psr18ReactBrowserClientTest.php
@@ -5,24 +5,25 @@ namespace Functional;
 
 use Nyholm\Psr7\Factory\Psr17Factory;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 use Symfony\Component\Process\Process;
 use Veewee\Psr18ReactBrowser\Psr18ReactBrowserClient;
 
-class Psr18ReactBrowserClientTest extends TestCase
+final class Psr18ReactBrowserClientTest extends TestCase
 {
-    /** @test */
-    public function it_can_send_request_over_react_browser(): void
+    
+    public function test_it_can_send_request_over_react_browser(): void
     {
         $client = Psr18ReactBrowserClient::default();
         $factory = new Psr17Factory();
 
-        $this->onServer(static function () use ($client, $factory) {
+        $this->onServer(function () use ($client, $factory) {
             $response = $client->sendRequest(
-                $factory->createRequest('GET', 'http://127.0.0.1:8000/success.json')
+                $factory->createRequest('GET', 'http://127.0.0.1:8888/success.json')
             );
 
             self::assertSame(200, $response->getStatusCode());
-            self::assertSame('{"success": true}', (string) $response->getBody());
+            self::assertStringEqualsFile($this->serverLocation('/success.json'), (string) $response->getBody());
         });
     }
 
@@ -31,14 +32,25 @@ class Psr18ReactBrowserClientTest extends TestCase
      */
     private function onServer(callable $run)
     {
-        $process = Process::fromShellCommandline('composer functional-testserver');
+        $process = Process::fromShellCommandline('php -S 127.0.0.1:8888 -t '.$this->serverLocation());
         $process->start();
-        $process->waitUntil(static fn () => true);
+        $started = $process->waitUntil(static function ($type, $output) {
+            return str_contains($output, 'started');
+        });
+
+        if (!$started) {
+            throw new RuntimeException('Unable to start HTTP server');
+        }
 
         try {
             $run();
         } finally {
             $process->stop();
         }
+    }
+
+    private function serverLocation(string $path = ''): string
+    {
+        return FIXTURE_DIR.'/functional/server'.$path;
     }
 }
